bd3f449601894ae621602884c2b0b4de83abceca
wangyan <627408117@qq.com>
2016-06-13 17:48:26 +0800

    动弹模块的完成

---
+++ app/src/main/java/custom/TweetTextView.java
@@ -0,0 +1,26 @@
+package custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2016/6/13.
+ */
+public class TweetTextView extends TextView
+{
+
+    public TweetTextView(Context context) {
+        super(context);
+    }
+
+    public TweetTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TweetTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
